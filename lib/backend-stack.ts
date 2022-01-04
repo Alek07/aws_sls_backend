2d02a41c14d386f8f72e526efd3d60261b3b4687
Alek Rutherford <alekrutherford@Aleks-MacBook-Pro.local>
2022-01-04 12:17:49 -0500

    node lambda function and api gateway integration added

--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -1,13 +1,19 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 import {
   Code,
   Function as LambdaFunction,
   Runtime,
 } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 import { join } from "path";
+import { GenericTable } from "./GenericTable";
 
 export class BackendStack extends Stack {
+  private api = new RestApi(this, "SpaceFinderAPI");
+  private spacesTable = new GenericTable("SpacesTable", "spaceId", this);
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
@@ -16,5 +22,15 @@ export class BackendStack extends Stack {
       code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
       handler: "index.main",
     });
+
+    const helloLambdaNodeJS = new NodejsFunction(this, "helloLambdaNodejs", {
+      entry: join(__dirname, "..", "services", "node-lambda", "hello.ts"),
+      handler: "handler",
+    });
+
+    // API Gateway integration
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource("hello");
+    helloLambdaResource.addMethod("GET", helloLambdaIntegration);
   }
 }
