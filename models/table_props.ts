020e3d90d1dbb3f7b32cf5829947d4a74d100ecf
Alek Rutherford <alekrutherford@Aleks-MacBook-Pro.local>
2022-01-26 18:02:14 -0500

    table props interface created

---
+++ models/table_props.ts
@@ -0,0 +1,8 @@
+export interface TableProps {
+    tableName: string,
+    primaryKey: string,
+    createLambdaPath?: string,
+    readLambdaPath?: string,
+    updateLambdaPath?: string,
+    deleteLambdaPath?: string,
+}
