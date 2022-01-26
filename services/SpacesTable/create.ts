0e8cf36cfae129d052ee8fc1d64bdc539d971d83
Alek Rutherford <alekrutherford@Aleks-MacBook-Pro.local>
2022-01-26 18:01:22 -0500

    file renamed

---
+++ services/SpacesTable/create.ts
@@ -0,0 +1,43 @@
+import { DynamoDB } from "aws-sdk";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
+import { v4 } from "uuid";
+
+const TABLE_NAME = process.env.TABLE_NAME;
+const dbClient = new DynamoDB.DocumentClient();
+
+async function handler(
+  event: APIGatewayProxyEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> {
+  const result: APIGatewayProxyResult = {
+    statusCode: 200,
+    body: "Hello from DYnamoDb",
+  };
+
+  const item =
+    typeof event.body === "object" ? event.body : JSON.parse(event.body);
+  item.spaceId = v4();
+
+  try {
+    await dbClient
+      .put({
+        TableName: TABLE_NAME!,
+        Item: item,
+      })
+      .promise();
+  } catch (error) {
+    if (error instanceof Error) {
+      result.body = error.message;
+    }
+  }
+
+  result.body = JSON.stringify(`Created item with id: ${item.spaceId}`);
+
+  return result;
+}
+
+export { handler };
