cd2320bd686db81be02bdb5b6b0199f0b9d4ffeb
Alek Rutherford <alekrutherford@Aleks-MacBook-Pro.local>
2022-01-28 15:28:20 -0500

    lambda get method added to space resource

--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -13,6 +13,7 @@ export class BackendStack extends Stack {
     tableName: "SpacesTable",
     primaryKey: "spaceId",
     createLambdaPath: "create",
+    readLambdaPath: "read",
   });
 
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -36,5 +37,6 @@ export class BackendStack extends Stack {
     //Spaces API integrations:
     const spaceResource = this.api.root.addResource("spaces");
     spaceResource.addMethod("POST", this.spacesTable.createLambdaIntegration);
+    spaceResource.addMethod("GET", this.spacesTable.readLambdaIntegration);
   }
 }
