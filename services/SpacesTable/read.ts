a77893203c12d97d49b1fcd01437dfc1a88787d7
Alek Rutherford <alekrutherford@Aleks-MacBook-Pro.local>
2022-01-28 15:27:29 -0500

    dynamo read handler setup

---
+++ services/SpacesTable/read.ts
@@ -0,0 +1,56 @@
+import { DynamoDB } from "aws-sdk";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
+
+const TABLE_NAME = process.env.TABLE_NAME;
+const PRIMARY_KEY = process.env.PRIMARY_KEY;
+const dbClient = new DynamoDB.DocumentClient();
+
+async function handler(
+  event: APIGatewayProxyEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> {
+  const result: APIGatewayProxyResult = {
+    statusCode: 200,
+    body: "Hello from DynamoDb",
+  };
+
+  try {
+    if (event.queryStringParameters) {
+      if (PRIMARY_KEY! in event.queryStringParameters) {
+        const keyValue = event.queryStringParameters[PRIMARY_KEY!];
+        const queryResponse = await dbClient
+          .query({
+            TableName: TABLE_NAME!,
+            KeyConditionExpression: "#id = :id",
+            ExpressionAttributeNames: {
+              "#id": PRIMARY_KEY!,
+            },
+            ExpressionAttributeValues: {
+              ":id": keyValue,
+            },
+          })
+          .promise();
+        result.body = JSON.stringify(queryResponse);
+      }
+    } else {
+      const queryResponse = await dbClient
+        .scan({
+          TableName: TABLE_NAME!,
+        })
+        .promise();
+      result.body = JSON.stringify(queryResponse);
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      result.body = error.message;
+    }
+  }
+
+  return result;
+}
+
+export { handler };
