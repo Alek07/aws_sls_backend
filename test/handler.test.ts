b849ec8ac0519074b4e3b2b3e7ee9b51966e088c
Alek Rutherford <alekrutherford@Aleks-MacBook-Pro.local>
2022-01-31 17:18:49 -0500

    secondary indexes test added

--- test/handler.test.ts
+++ test/handler.test.ts
@@ -3,11 +3,16 @@ import { handler } from "../services/SpacesTable/read";
 
 const event: APIGatewayProxyEvent = {
   queryStringParameters: {
-    spaceId: "36594f21-d626-4843-afe7-f951cdd3fe52",
+    // spaceId: "36594f21-d626-4843-afe7-f951cdd3fe52",
+    location: "Panama City",
   },
 } as any;
 
-const result = handler({} as any, {} as any).then((apiResult) => {
-  const items = JSON.parse(apiResult.body);
-  console.log("parse response!");
-});
+try {
+  const result = handler(event, {} as any).then((apiResult) => {
+    const items = JSON.parse(apiResult.body);
+    console.log("parse response!");
+  });
+} catch (error) {
+  console.log(error);
+}
