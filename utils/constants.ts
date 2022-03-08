c3f1ca69e8b3998961537aac02afb90718e6e95e
Alek Rutherford <alekrutherford@Aleks-MacBook-Pro.local>
2022-03-08 15:12:43 -0500

    space scheme created

---
+++ utils/constants.ts
@@ -0,0 +1,9 @@
+import { Space } from "../models/space";
+
+export const SpaceScheme: Space = {
+  spaceId: "string",
+  location: "string",
+  name: "string",
+  score: 0,
+  photoUrl: "optional",
+};
