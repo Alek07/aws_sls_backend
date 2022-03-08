88f10d17ca0aa67bcec73b1d3fe7480c49f79910
Alek Rutherford <alekrutherford@Aleks-MacBook-Pro.local>
2022-03-08 15:10:34 -0500

    dynamo delete handler setup

---
+++ services/SpacesTable/delete.ts
@@ -0,0 +1,46 @@
+import { DynamoDB } from "aws-sdk";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
+import { v4 } from "uuid";
+
+const TABLE_NAME = process.env.TABLE_NAME as string;
+const PRIMARY_KEY = process.env.PRIMARY_KEY as string;
+const dbClient = new DynamoDB.DocumentClient();
+
+const handler = async (
+  event: APIGatewayProxyEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> => {
+  const result: APIGatewayProxyResult = {
+    statusCode: 200,
+    body: "Hello from DynamoDb",
+  };
+
+  try {
+    const spaceId = event.queryStringParameters?.[PRIMARY_KEY];
+
+    if (spaceId) {
+      const deleteResult = await dbClient
+        .delete({
+          TableName: TABLE_NAME,
+          Key: {
+            [PRIMARY_KEY]: spaceId,
+          },
+        })
+        .promise();
+
+      result.body = JSON.stringify(deleteResult);
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      result.body = error.message;
+    }
+  }
+
+  return result;
+};
+
+export { handler };
