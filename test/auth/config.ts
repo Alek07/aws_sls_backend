d32c9f1ddd841e3f681e82890c09bb2d4d424d78
Alek07 <alekissac@gmail.com>
2022-03-11 12:01:13 -0500

    authorizer test config and class

---
+++ test/auth/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  REGION: "eu-west-2",
+  USER_POOL_ID: "us-west-2_ytbkOhQMG",
+  APP_CLIENT_ID: "3s8hj5t1dvl2qqmpal7mj3idpl",
+  // IDENTITY_POOL_ID: "eu-west-1:c1490135-4dd2-4a82-9959-880f77fef599",
+  TEST_USER_NAME: "alek01",
+  TEST_USER_PASSWORD: "@Alek071298",
+};
