7b436387c014af9524e1e7fa12fbe270bac9b0d2
Alek07 <alekissac@gmail.com>
2022-03-14 11:26:10 -0500

    identity pool roles and user groups test added

--- test/auth/auth.test.ts
+++ test/auth/auth.test.ts
@@ -2,6 +2,18 @@ import * as AWS from "aws-sdk";
 import { AuthService } from "./AuthService";
 import { config } from "./config";
 
+AWS.config.region = config.REGION;
+
+async function getBuckets(){
+    let buckets;
+    try {
+        buckets = await new AWS.S3().listBuckets().promise();
+    } catch (error) {
+        buckets = undefined
+    }
+    return buckets
+}
+
 async function authServiceTest() {
   const authService = new AuthService();
 
@@ -12,6 +24,7 @@ async function authServiceTest() {
 
   await authService.getAWSTemporaryCreds(user);
   const someCreds = AWS.config.credentials;
+  const buckets = await getBuckets();
   console.log(someCreds);
 }
 
