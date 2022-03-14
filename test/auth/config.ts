52abc9667fa5a4913c784889d88601be9fa2376d
Alek07 <alekissac@gmail.com>
2022-03-14 11:25:47 -0500

    new identity pool id added

--- test/auth/config.ts
+++ test/auth/config.ts
@@ -2,7 +2,7 @@ export const config = {
   REGION: "us-west-2",
   USER_POOL_ID: "us-west-2_ytbkOhQMG",
   APP_CLIENT_ID: "3s8hj5t1dvl2qqmpal7mj3idpl",
-  IDENTITY_POOL_ID: "us-west-2:6c5b5ed6-055e-4728-96b3-a0dc14c2f849",
+  IDENTITY_POOL_ID: "us-west-2:844e084f-21f6-480c-9ea9-95a7dfdd09cd",
   TEST_USER_NAME: "alek01",
   TEST_USER_PASSWORD: "@Alek071298",
 };
