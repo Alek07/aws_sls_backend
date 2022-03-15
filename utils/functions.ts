25b601ad666e75951c237478635c4b901f2fd166
Alek07 <alekissac@gmail.com>
2022-03-15 14:12:19 -0500

    add cors header created

--- utils/functions.ts
+++ utils/functions.ts
@@ -1,4 +1,4 @@
-import { APIGatewayProxyEvent } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
 // Check if scheme property is optional
 export const isOptional = (value: any) => {
@@ -8,3 +8,11 @@ export const isOptional = (value: any) => {
 export const getEventBody = (event: APIGatewayProxyEvent) => {
   return typeof event.body === "object" ? event.body : JSON.parse(event.body);
 };
+
+export function addCorsHeader(result: APIGatewayProxyResult) {
+  result.headers = {
+    "Content-type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+  };
+}
